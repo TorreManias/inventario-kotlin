f84535abf305ae7f2f3151f7517934324a0d3b67
TorreManias <correosinnombre02@gmail.com>
2021-11-16 09:12:34 -0600

    Edicion de productos

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -49,6 +49,30 @@ class InventoryViewModel (private val itemDao: ItemDao): ViewModel() {
         }
     }
 
+    fun updateItem(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ) {
+        val updatedItem = getUpdatedItemEntry(itemId, itemName, itemPrice, itemCount)
+        updateItem(updatedItem)
+    }
+
+    private fun getUpdatedItemEntry(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ): Item {
+        return Item(
+            id = itemId,
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
     fun sellItem(item: Item) {
         if (item.quantityInStock > 0) {
             val newItem = item.copy(quantityInStock = item.quantityInStock - 1)
